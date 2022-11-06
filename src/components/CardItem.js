1a1f3cb28a54d84f8f74f9bb810bd581e6ad342f
Josh <hashimotojosh@gmail.com>
2022-11-06 08:45:26 -0800

    Version 2

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -10,7 +10,7 @@ function CardItem({ img, name, title }) {
       <div className='card-img sm:mb-4 md:mb-8'>
         <img className='md:rounded-2xl sm:max-w-3xs' src={img} alt={name}/>
       </div>
-      <p className='font-medium text-3xl text-red-900'>{name}</p>
+      <p className='font-medium text-3xl text-gray-400'>{name}</p>
       
     </div>
   );
